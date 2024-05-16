35c7c3d3e9172a6c9b4fb5b2ff6e0b44c4941cd6
zam <zamzam.saeful@efishery.com>
2024-05-16 07:50:43 +0700

    refactor: var name

--- solution.py
+++ solution.py
@@ -3,11 +3,6 @@ from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 import matplotlib.pyplot as plt
 import numpy as np
 
-def load_data(file_path):
-    return pd.read_csv(file_path)
-
-def extract_fingerprints(data):
-    return data.iloc[:, 1:].values
 
 def cluster_fingerprints(data, labels):
     linked = linkage(data, method='single', metric='euclidean')
@@ -17,12 +12,22 @@ def cluster_fingerprints(data, labels):
     clusters = fcluster(linked, 3, criterion='maxclust')
     return clusters
 
+def to_matrix(data):
+    return data.iloc[:, 1:].values
+
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
 def main():
     data = load_data("./data/result_smile_descriptor.csv")
     print(data.head())
     labels = data.iloc[:, 0].values
-    fingerprints = extract_fingerprints(data)
-    clusters = cluster_fingerprints(fingerprints, labels)
+    
+    matrix = to_matrix(data)
+    
+    
+    
+    clusters = cluster_fingerprints(matrix, labels)
     data["Cluster"] = clusters
     print("========== Hasil Cluter ==========")
     sorted_data = data.sort_values(by=['Cluster'], ascending=True)
